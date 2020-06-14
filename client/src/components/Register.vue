6699c67f12b48588de16f11e1ceaa48695336993
Brandon C. Brown <brandon.c.brown@outlook.com>
2020-06-13 23:42:40 -0400

    day 2.2

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,6 +6,8 @@
     <br>
     <input type="password" name="password" v-model="password" placeholder="password"/>
     <br>
+    <div class="error" v-html="error" />
+    <br>
     <button @click="register">Register</button>
   </div>
 </template>
@@ -17,7 +19,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
@@ -37,5 +40,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: crimson;
+}
 </style>
